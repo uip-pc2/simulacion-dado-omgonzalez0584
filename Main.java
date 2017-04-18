d5c1fa9233111e57ba1dca82bca8aacda8cd04fb
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-04-17 21:43:56 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	 int i = 0;
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+	 do{
+	     System.out.println("\n Simulacion de dado: ");
+         System.out.println("\n Escoja una de las siguientes opciones");
+         System.out.println("\n Tirar Dado..................1 ");
+         System.out.println("\n Ver resultado...............2 ");
+         System.out.println("\n Ver todos los resultados....3 ");
+         System.out.println("\n Salir.......................4 ");
+         System.out.println("\n: ");
+         try {
+             i = Integer.parseInt(teclado.readLine());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         switch(i){
+             case 1:
+                 break;
+
+             case 2:
+                 break;
+
+             case 3:
+                  break;
+
+             case 4:
+                  break;
+
+              default:
+                  break;
+
+
+
+         }
+
+     }while(i!=4);
+
+    }
+}
