ffb1c6f07dac528a1c29f60bb63519f4ab41ab11
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-04-20 19:03:40 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,49 +7,47 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
-     Dado l = new Dado();
-	 int i = 0;
-	 int j=0;
-	 double res;
+        Dado l = new Dado();
+        int i = 0;
+        int j = 0;
+        int res;
         BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        do{
+            System.out.println("\n Simulacion de dado: ");
+            System.out.println("\n Escoja una de las siguientes opciones");
+            System.out.println("\n Tirar Dado..................1 ");
+            System.out.println("\n Ver resultado...............2 ");
+            System.out.println("\n Ver todos los resultados....3 ");
+            System.out.println("\n Salir.......................4 ");
+            System.out.println("\n: ");
+            try {
+                i = Integer.parseInt(teclado.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            switch (i) {
+                case 1:
+                    res = l.lanza_dado();
+                    l.imprimir_resultado(res);
+                    break;
+                case 2:
+                    break;
 
-	 do{
-	     System.out.println("\n Simulacion de dado: ");
-         System.out.println("\n Escoja una de las siguientes opciones");
-         System.out.println("\n Tirar Dado..................1 ");
-         System.out.println("\n Ver resultado...............2 ");
-         System.out.println("\n Ver todos los resultados....3 ");
-         System.out.println("\n Salir.......................4 ");
-         System.out.println("\n: ");
-         try {
-             i = Integer.parseInt(teclado.readLine());
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-         switch(i){
-             case 1:
-                 res=l.lanza_dado();
+                case 3:
+                    break;
 
-                 break;
+                case 4:
+                    break;
 
-
-             case 2:
-                 break;
-
-             case 3:
-                  break;
-
-             case 4:
-                  break;
-
-              default:
-                  break;
+                default:
+                    break;
 
 
+            }
 
-         }
+        } while (i != 4);
 
-     }while(i!=4);
 
     }
 }
+
