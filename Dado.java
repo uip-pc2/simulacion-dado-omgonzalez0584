98d01b9e13c88512da85b9ea3b664aefd149102c
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-04-23 12:10:00 -0500

    Add files via upload

--- Dado.java
+++ Dado.java
@@ -1,15 +1,30 @@
 package app;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class Dado {
+
 
     public int lanza_dado() {
         double resultado;
         resultado = Math.random() * 6;
         return(int) resultado;
     }
-    public void imprimir_resultado(int res){
-          System.out.print("El resultado es: "+ res);
+    public void imprimir_resultado(int j) {
+        System.out.print("El resultado es: " + j);
+
     }
+    public void imprimir_todos_resultados(ArrayList ar){
+        int i;
+        for(i=0; ar.size() > i; i++){
+            System.out.println(ar.get(i));
+        }
+
+    }
+
+
+
+
+
 }
